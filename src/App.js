da7fe90187a8de29f488dc68b709e21224998dc0
Karthik <33004854+Karthi1908@users.noreply.github.com>
2023-01-09 21:59:53 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
       //   console.log(key);
       //   console.log(storage.predictTokenDetails.get(key));
       // }
-      for (let pred =22; pred < 47 ; pred++  ) {
+      for (let pred =22; pred < 61 ; pred++  ) {
 	  	 await predictions.get(pred).then(value => { predList.push({ id: pred, value }) })
 	      					.catch( console.log(pred , " not in the list"));
 		 
